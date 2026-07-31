0cfc3d948fac89a75d2e76428d516d577ea216d9
gautam342 <gautamkne35@gmail.com>
2026-07-31 22:19:12 +0530

    Add merge sort implementation in mergesort.c
    Implement merge sort algorithm with print functionality.

---
+++ assignment_4/mergesort.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+void printarray(int *A,int n){
+    for(int i=0;i<n;i++){
+        printf("%d  ",A[i]);
+    }
+    printf("\n");
+}
+void merge(int A[],int mid,int low, int high){
+    int i,j,k,B[100];
+    i=low;
+    j=mid+1;
+    k=low;
+    while(i<=mid && j<=high){
+        if(A[i]<A[j]){
+            B[k]=A[i];
+            i++;k++;
+        }
+        else{
+          B[k]=A[j];
+          j++;k++;
+        }
+    }
+
+    while(i<=mid ){
+        
+            B[k]=A[i];
+            k++;i++;
+        }
+
+    while(j<=high ){
+        
+            B[k]=A[j];
+            k++;j++;
+        } 
+        for(int i=low;i<=high;i++){
+            A[i]=B[i];
+        }
+}
+void mergesort(int A[],int low ,int high){
+    int mid;
+    if(low<high){
+        mid=(low+high)/2;
+        mergesort(A, low, mid);
+         mergesort(A, mid+1, high);
+         merge(A, mid, low, high);
+
+    }
+}
+
+int main(){
+    int A[]={1,2,5,3,4};
+    int n=5;
+    printarray(A,n);
+    mergesort(A,0,n-1);
+    printarray(A,n);
+
+    return 0;
+}
